ca42de420a2db31cfa9bc6ca8fd938697329f73e
AaronChuang <aaron@ehanlin.com.tw>
2019-01-07 18:37:45 +0800

    即時預覽初版

---
+++ Assets/Scripts/Editor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Editor : MonoBehaviour
+{
+    [SerializeField] InputField editText;
+    [SerializeField] TextMeshProUGUI previewText;
+
+    public void OnValueChange()
+    {
+        previewText.text = editText.text;
+    }
+
+}
